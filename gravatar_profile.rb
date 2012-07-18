4382f8507d9c1d55c72c5b0086f2154f81b3014c
Tom Wilson <tom@jackhq.com>
2012-07-18 15:33:11 -0400

    happy path

---
+++ gravatar_profile.rb
@@ -0,0 +1,13 @@
+require 'digest/md5'
+require 'rest_client'
+require 'json'
+
+class GravatarProfile
+  attr_accessor :name, :photo, :url
+  def initialize(email)
+    user_hash = Digest::MD5.hexdigest(email)
+    response = JSON.parse(RestClient.get("http://gravatar.com/#{user_hash}.json"))
+    profile = response['entry'].first
+    @name, @photo, @url = profile['displayName'], profile['photos'].first['value'], profile['profileUrl']
+  end
+end
---
+++ spec/gravatar_profile_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../gravatar_profile'
+
+describe GravatarProfile do
+  subject { GravatarProfile.new('tom@jackhq.com') }
+  it 'should be true' do
+    subject.should be_true
+  end
+
+  it 'should get displayName' do
+    subject.name.should == 'tjackhq'
+  end
+
+  it 'should get photo' do
+    subject.photo.should == 'http://0.gravatar.com/avatar/ebd63577f6cef101bf22245572781d5d'
+  end
+
+  it 'should get profile url' do
+    subject.url.should == 'http://gravatar.com/tjackhq'
+  end
+end
